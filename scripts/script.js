bca3c58fb1f726f1103a857b38bb31de29c1f08f
fabioDev21 <fabiotwist1@gmail.com>
2025-02-08 07:05:12 -0300

    função de update atualizada! Abre janela na tela

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', e => {
 
 function criaElementoTela({value: texto}){
     const idLiElemento = Date.now()
-    const elementoLiParaMostrarNaTela = `<li class="listElementoNum${idLiElemento}"><button id="editBtn">Editar</button><p>${texto}</p><button id="deleteBtn">Deletar</button></li>`
+    const elementoLiParaMostrarNaTela = `<li class="listElementoNum${idLiElemento}"><button id="editBtn">Editar</button><p id="textElement">${texto}</p><button id="deleteBtn">Deletar</button></li>`
 
     const listElementos = document.querySelector('ul')
     listElementos.insertAdjacentHTML('afterbegin', elementoLiParaMostrarNaTela)
@@ -34,15 +34,32 @@ const editBoxAserAdc =
         <label for="novoItemSetado">
             <p>Novo item: </p>
             <input id="novoItemSetado" type="text">
-        </label>
-        <p id="closeBtn">Fechar</p>
+            </label>
+            <button type='submit' id="submitEditbtn" >Enviar</button>
+        <p id="editCloseBtn">Fechar</p>
     </div>`
 // Elemento a ser finalizado, caixa de edição de itens
 
 function editaElementTela(idLiElemento){
-    const liElemento = document.querySelector('.listElementoNum' + idLiElemento)
-    const textoToUpdate = liElemento.childNodes[1]
-    textoToUpdate.textContent = prompt("Qual o novo item?")
+    const elementText = document.querySelector('#textElement')
+    
+    // adicionando a box no body do html
+    const corpoHTML = document.querySelector('body')
+    corpoHTML.insertAdjacentHTML('beforeend', editBoxAserAdc)
+    
+    // escutador de eventos para o botão de fechar a caixa, ele remove o elemento e dados da tela tbm
+    const editCloseBtn = document.querySelector('#editCloseBtn')
+    editCloseBtn.addEventListener('click', () => {
+        document.querySelector('#editBox').remove()
+    })
+
+    // Envia o input para atualizar o elemento
+    const submitEditbtn = document.querySelector('#submitEditbtn')
+    submitEditbtn.addEventListener('click', () => {
+        const novoItemSetado = document.querySelector("#novoItemSetado").value
+        elementText.textContent = novoItemSetado        
+        document.querySelector('#editBox').remove()
+    })
 }
 
 function deletaElementoTela(idLiElemento){
